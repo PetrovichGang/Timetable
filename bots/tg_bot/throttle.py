d586aa80abb547fe9af9c0b115b665208f3aa452
WEx <>
2021-10-26 11:22:00 +0800

    TG: изменен throttle

--- bots/tg_bot/throttle.py
+++ bots/tg_bot/throttle.py
@@ -1,22 +1,33 @@
 from aiogram.dispatcher import middlewares, handler
+from aiogram.dispatcher.dispatcher import DEFAULT_RATE_LIMIT
 from aiogram.utils.exceptions import Throttled
 from bots.common.strings import strings
+from aiogram.dispatcher.handler import CancelHandler, current_handler
 from aiogram import types, Dispatcher
 import asyncio
 
 
 class ThrottlingMiddleware(middlewares.BaseMiddleware):
-    def __init__(self, dispatcher: Dispatcher):
-        self.dispatcher = dispatcher
+    def __init__(self, limit=DEFAULT_RATE_LIMIT, key_prefix='antiflood_'):
+        self.rate_limit = limit
+        self.prefix = key_prefix
         super(ThrottlingMiddleware, self).__init__()
 
-    # noinspection PyUnusedLocal
     async def on_process_message(self, message: types.Message, data: dict):
+        handler = current_handler.get()
+        dispatcher = Dispatcher.get_current()
+        if handler:
+            limit = getattr(handler, "throttling_rate_limit", self.rate_limit)
+            key = getattr(handler, "throttling_key", f"{self.prefix}_{handler.__name__}")
+        else:
+            limit = self.rate_limit
+            key = f"{self.prefix}_message"
         try:
-            await self.dispatcher.throttle("throttle", rate=10)  # сброс состояния через x сек.
+            await dispatcher.throttle(key, rate=limit)
         except Throttled as t:
-            if t.exceeded_count == 20:  # кол-во сообщений за x сек.
-                await message.answer(strings.error.throttle)
-            if t.exceeded_count >= 20:
-                await asyncio.sleep(10)
-                raise handler.CancelHandler()
+            await self.message_throttled(message, t)
+            raise CancelHandler()
+
+    async def message_throttled(self, message: types.Message, throttled: Throttled):
+        if throttled.exceeded_count <= 2:
+            await message.reply("Too many requests!")
