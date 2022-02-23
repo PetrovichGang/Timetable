ea1ea521a10e96e2c25ed8aa5024206860093a54
dmitrynuke <>
2022-02-23 20:45:04 +0800

    Статистика для админ-панели

--- app/api/vk.py
+++ app/api/vk.py
@@ -9,6 +9,7 @@ from typing import List, Union
 from starlette import status
 from ..utils import db
 from ..utils.etc import unix_to_date
+from datetime import datetime
 
 routerPrivateVK = APIRouter(prefix="/api/vk")
 
@@ -152,3 +153,49 @@ async def get_statistics():
                            f" {unix_to_date(u['join'])}, {u['lesson_group']}"
                            for i, u in enumerate(last_10)])
     return Response(response, status_code=status.HTTP_200_OK)
+
+
+
+@routerPrivateVK.get("/statistics_all",
+                     summary="Получение статистики VK в формате json",
+                     tags=["VK"])
+async def get_statistics():
+    users = await TimeTableDB.async_iteration(db.VKUsersCollection.find({}, {
+        "lesson_group": 1, "join": 1, "first_name": 1, "last_name": 1, "peer_id": 1, "_id": 0
+    }).sort('join', 1))
+    
+    user_graphic = {}
+    groups = {}
+    groups_full = {}
+    user_count = 0
+
+    for row in users:
+        less_grp = row['lesson_group'] if not row['lesson_group'] is None else 'Не задано'
+        letter = less_grp[0]
+        if row['lesson_group'] == None:
+            letter = 'Не задано'
+        if letter in groups:
+            groups[letter] += 1
+        else:
+            groups[letter] = 1
+            
+        if row['lesson_group'] in groups_full:
+            groups_full[row['lesson_group']] += 1
+        else:
+            groups_full[row['lesson_group']] = 1
+        
+        user_count += 1
+        day = datetime.utcfromtimestamp(int(row['join'])).strftime('%Y-%m-%d')
+        user_graphic[day] = user_count
+            
+    return JSONResponse({
+        'users': {
+            'count': len(users),
+            'chart': list(user_graphic.items()),
+            'full': users[-10:],
+        },
+        'groups': {
+            'chart': groups,
+            'full': groups_full,
+        }
+    }, status_code=status.HTTP_200_OK)
